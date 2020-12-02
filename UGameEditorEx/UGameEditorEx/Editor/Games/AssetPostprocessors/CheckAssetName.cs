7738e73652b478860ada49e56920ab6f450f81e5
龚阳辉 <gongyanghui@dianyuegame.com>
2020-12-02 11:38:55 +0800

    修改下逻辑判断

--- UGameEditorEx/UGameEditorEx/Editor/Games/AssetPostprocessors/CheckAssetName.cs
+++ UGameEditorEx/UGameEditorEx/Editor/Games/AssetPostprocessors/CheckAssetName.cs
@@ -11,7 +11,7 @@ public class CheckAssetName : AssetPostprocessor
 {
     static bool IsExcludes(string strTower)
     {
-        return strTower.EndsWith(".cs") || strTower.EndsWith(".meta") || strTower.EndsWith(".shader") || strTower.EndsWith(".tga") || strTower.EndsWith(".fbx") || strTower.EndsWith(".rendertexture") || strTower.Contains("lightmap");
+        return strTower.EndsWith(".cs") || strTower.EndsWith(".meta") || strTower.EndsWith(".shader") || strTower.EndsWith(".tga") || strTower.EndsWith(".fbx") || strTower.EndsWith(".rendertexture") || strTower.Contains("/lightmaps/");
     }
 
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
@@ -19,37 +19,38 @@ public class CheckAssetName : AssetPostprocessor
         CheckAllAssets(importedAssets);
     }
 
-    static void CheckAllAssets(string[] importedAssets)
+    static public void CheckAllAssets(string[] importedAssets)
     {
         int nLens = 0;
         if (importedAssets != null) nLens = importedAssets.Length;
         if (nLens > 0)
         {
-            string _fn = null, _fnTower = null;
+            string _fn = null, _fnTower = null, _fpStr = null;
             foreach (string str in importedAssets)
             {
-                if (!str.Contains(BuildPatcher.m_rootRelative))
+                _fpStr = BuildPatcher.ReplaceSeparator(str);
+                if (!_fpStr.Contains(BuildPatcher.m_rootRelative))
                     continue;
 
-                if (BuildPatcher.IsFolder(_fn))
+                if (BuildPatcher.IsFolder(_fpStr))
                     continue;
 
-                _fn = Path.GetFileName(str);
+                _fn = Path.GetFileName(_fpStr);
                 _fnTower = _fn.ToLower();
-                if (str.Contains(" "))
+                if (_fpStr.Contains(" "))
                 {
-                    UnityEngine.Debug.LogErrorFormat("=== filename has space(空格), fp = [{0}],fn = [{1}]", str, _fn);
+                    UnityEngine.Debug.LogErrorFormat("=== filename has space(空格), fp = [{0}],fn = [{1}]", _fpStr, _fn);
                 }
-                else if (!IsExcludes(_fnTower) && !_fn.Equals(_fnTower))
+                else if (!IsExcludes(_fpStr) && !_fn.Equals(_fnTower))
                 {
-                    UnityEngine.Debug.LogErrorFormat("=== filename has Upper(大写), fp = [{0}],fn = [{1}]", str, _fn);
+                    UnityEngine.Debug.LogErrorFormat("=== filename has Upper(大写), fp = [{0}],fn = [{1}]", _fpStr, _fn);
                 }
             }
         }
     }
 
     [MenuItem("Tools/Check AllRes Format(检查所有资源的命名)")]
-    static void ReCheckAll()
+    static public void ReCheckAll()
     {
         string _fd = BuildPatcher.m_appAssetPath;
         string[] files = Directory.GetFiles(_fd, "*.*", SearchOption.AllDirectories);
