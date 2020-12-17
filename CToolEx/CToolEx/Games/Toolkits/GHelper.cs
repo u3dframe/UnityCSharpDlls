3cd862414d2b36daff3cac39b94fce32e6dfe4a1
龚阳辉 <gongyanghui@dianyuegame.com>
2020-12-17 10:09:44 +0800

    格式化

--- CToolEx/CToolEx/Games/Toolkits/GHelper.cs
+++ CToolEx/CToolEx/Games/Toolkits/GHelper.cs
@@ -10,93 +10,107 @@ using UObject = UnityEngine.Object;
 /// 日期 : 2020-06-26 09:33
 /// 功能 : 泛型是不能被Tolua导成函数的
 /// </summary>
-public class GHelper {
-	static public bool Is_App_Quit = false;
-	static public readonly DateTime DT_Start = new DateTime(1970, 1, 1, 0, 0, 0);
+public class GHelper
+{
+    static public bool Is_App_Quit = false;
+    static public readonly DateTime DT_Start = new DateTime(1970, 1, 1, 0, 0, 0);
 
-	static public DateTime ToTZLoc{
-		get{
+    static public DateTime ToTZLoc
+    {
+        get
+        {
             // return TimeZone.CurrentTimeZone.ToLocalTime(DT_Start);
             return TimeZoneInfo.ConvertTime(DT_Start, TimeZoneInfo.Local);
-		} 
-	}
-	
-	static public long TickStartUtc{
-		get{
-			return ToTZLoc.ToUniversalTime().Ticks;
-		}
-	}
-	
-	static public long TickStartLoc{
-		get{
-			return ToTZLoc.ToLocalTime().Ticks;
-		}
-	}
+        }
+    }
 
-	/// <summary>
-	/// getType
-	/// </summary>
-	/// <param name="classname"></param>
-	/// <returns></returns>
-	static public System.Type GetType(string classname) {
-		Assembly assb = Assembly.GetExecutingAssembly();  //.GetExecutingAssembly();
-		System.Type t = null;
-		t = assb.GetType(classname);
-		return t;
-	}
-	
-	static public long GetTime() {
-		TimeSpan ts = new TimeSpan(DateTime.UtcNow.Ticks - TickStartUtc);
-		return (long)ts.TotalMilliseconds;
-	}
+    static public long TickStartUtc
+    {
+        get
+        {
+            return ToTZLoc.ToUniversalTime().Ticks;
+        }
+    }
 
-	static public void ThrowError(string msg) {
-		throw new Exception(msg);
-	}
+    static public long TickStartLoc
+    {
+        get
+        {
+            return ToTZLoc.ToLocalTime().Ticks;
+        }
+    }
 
-	static public bool IsNull(UObject uobj)
-	{
-		if(Is_App_Quit) return true;
-		return null == uobj;
-	}
+    /// <summary>
+    /// getType
+    /// </summary>
+    /// <param name="classname"></param>
+    /// <returns></returns>
+    static public System.Type GetType(string classname)
+    {
+        Assembly assb = Assembly.GetExecutingAssembly();  //.GetExecutingAssembly();
+        System.Type t = null;
+        t = assb.GetType(classname);
+        return t;
+    }
 
-	static public bool IsNoNull(UObject uobj)
-	{
-		if(Is_App_Quit) return false;
-		return null != uobj;
-	}
-	
-	// 在编辑模式下，这个函数有问题，即便为null对象，经过判断就不为空了
-	static public bool IsNull(object obj)
-	{
-		return object.ReferenceEquals(obj,null);
-	}
+    static public long GetTime()
+    {
+        TimeSpan ts = new TimeSpan(DateTime.UtcNow.Ticks - TickStartUtc);
+        return (long)ts.TotalMilliseconds;
+    }
 
-	static public bool IsNoNull(object obj)
-	{
-		return !IsNull(obj);
-	}
+    static public void ThrowError(string msg)
+    {
+        throw new Exception(msg);
+    }
 
-	static public bool IsComponent(object obj) {
-		if(IsNull(obj))	return false;
-		return obj is Component;
-	}
+    static public bool IsNull(UObject uobj)
+    {
+        if (Is_App_Quit) return true;
+        return null == uobj;
+    }
 
-	static public bool IsCamera(object obj) {
-		if(IsNull(obj))	return false;
-		return obj is Camera;
-	}
+    static public bool IsNoNull(UObject uobj)
+    {
+        if (Is_App_Quit) return false;
+        return null != uobj;
+    }
 
-	static public bool IsTransform(object obj) {
-		if(IsNull(obj))	return false;
-		return obj is Transform;
-	}
+    // 在编辑模式下，这个函数有问题，即便为null对象，经过判断就不为空了
+    static public bool IsNull(object obj)
+    {
+        return object.ReferenceEquals(obj, null);
+    }
 
-	static public bool IsGameObject(object obj) {
-		if(IsNull(obj))	return false;
-		return obj is GameObject;
-	}
-    
+    static public bool IsNoNull(object obj)
+    {
+        return !IsNull(obj);
+    }
+
+    static public bool IsComponent(object obj)
+    {
+        if (IsNull(obj)) return false;
+        return obj is Component;
+    }
+
+    static public bool IsCamera(object obj)
+    {
+        if (IsNull(obj)) return false;
+        return obj is Camera;
+    }
+
+    static public bool IsTransform(object obj)
+    {
+        if (IsNull(obj)) return false;
+        return obj is Transform;
+    }
+
+    static public bool IsGameObject(object obj)
+    {
+        if (IsNull(obj)) return false;
+        return obj is GameObject;
+    }
+
     static public GameObject ToGObj(UObject uobj)
     {
         if (IsNull(uobj)) return null;
@@ -162,194 +176,229 @@ public class GHelper {
         return IsInLayerMask(uobj, layerMask);
     }
 
-    static public T Get<T>(UObject uobj) where T : Component {
+    static public T Get<T>(UObject uobj) where T : Component
+    {
         GameObject gobj = ToGObj(uobj);
         if (IsNull(gobj)) return null;
-		return gobj.GetComponent<T>();
-	}
+        return gobj.GetComponent<T>();
+    }
 
-	static public T Get<T>(UObject uobj, bool isAdd) where T : Component {
+    static public T Get<T>(UObject uobj, bool isAdd) where T : Component
+    {
         GameObject gobj = ToGObj(uobj);
         if (IsNull(gobj)) return null;
-		T _r = gobj.GetComponent<T> ();
-		if (isAdd && IsNull(_r)) {
-			_r = gobj.AddComponent<T> ();
-		}
-		return _r;
-	}
+        T _r = gobj.GetComponent<T>();
+        if (isAdd && IsNull(_r))
+        {
+            _r = gobj.AddComponent<T>();
+        }
+        return _r;
+    }
 
-	/// <summary>
-	/// 搜索子物体组件
-	/// </summary>
-	static public T Get<T>(UObject uobj, string subnode) where T : Component {
+    /// <summary>
+    /// 搜索子物体组件
+    /// </summary>
+    static public T Get<T>(UObject uobj, string subnode) where T : Component
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return null;
         Transform sub = trsf.Find(subnode);
         return Get<T>(sub);
     }
 
-	static public T Get<T>(UObject uobj, string subnode,bool isAdd) where T : Component {
+    static public T Get<T>(UObject uobj, string subnode, bool isAdd) where T : Component
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return null;
-		Transform sub = trsf.Find(subnode);
-		return Get<T>(sub,isAdd);
-	}
-    
-	static public T GetInParent<T>(UObject uobj) where T : Component {
+        Transform sub = trsf.Find(subnode);
+        return Get<T>(sub, isAdd);
+    }
+
+    static public T GetInParent<T>(UObject uobj) where T : Component
+    {
         GameObject gobj = ToGObj(uobj);
         if (IsNull(gobj)) return null;
-		T ret = gobj.GetComponent<T> ();
-		if (!ret) {
-			ret = gobj.GetComponentInParent<T> ();
-		}
-		return ret;
-	}
+        T ret = gobj.GetComponent<T>();
+        if (!ret)
+        {
+            ret = gobj.GetComponentInParent<T>();
+        }
+        return ret;
+    }
 
-	static public T GetInParentRecursion<T>(UObject uobj) where T : Component {
+    static public T GetInParentRecursion<T>(UObject uobj) where T : Component
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return null;
-		T ret = trsf.GetComponent<T> ();
-		if (ret != null) return ret;
-		return GetInParentRecursion<T>(trsf.parent);
-	}
+        T ret = trsf.GetComponent<T>();
+        if (ret != null) return ret;
+        return GetInParentRecursion<T>(trsf.parent);
+    }
 
-	/// <summary>
-	/// 添加组件
-	/// </summary>
-	static public T Add<T>(UObject uobj) where T : Component {
+    /// <summary>
+    /// 添加组件
+    /// </summary>
+    static public T Add<T>(UObject uobj) where T : Component
+    {
         GameObject gobj = ToGObj(uobj);
-        if (IsNoNull(gobj)) {
-			T[] ts = gobj.GetComponents<T>();
-			for (int i = 0; i < ts.Length; i++) {
-				if (ts[i] != null) GameObject.DestroyImmediate(ts[i]);
-			}
-			return gobj.AddComponent<T>();
-		}
-		return null;
-	}
+        if (IsNoNull(gobj))
+        {
+            T[] ts = gobj.GetComponents<T>();
+            for (int i = 0; i < ts.Length; i++)
+            {
+                if (ts[i] != null) GameObject.DestroyImmediate(ts[i]);
+            }
+            return gobj.AddComponent<T>();
+        }
+        return null;
+    }
 
-	/// <summary>
-	/// 递归查找子对象
-	/// </summary>
-	static public Transform ChildRecursion(UObject uobj, string subnode) {
+    /// <summary>
+    /// 递归查找子对象
+    /// </summary>
+    static public Transform ChildRecursionTrsf(UObject uobj, string subnode)
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return null;
-		if(trsf.name.Equals(subnode)) return trsf;
-		int lens = trsf.childCount;
-		Transform _ret = null; 
-		for(int i = 0; i < lens;i++){
-			_ret = ChildRecursion(trsf.GetChild(i),subnode);
-			if(_ret != null)
-				return _ret;
-		}
-		return null;
-	}
+        if (trsf.name.Equals(subnode)) return trsf;
+        int lens = trsf.childCount;
+        Transform _ret = null;
+        for (int i = 0; i < lens; i++)
+        {
+            _ret = ChildRecursionTrsf(trsf.GetChild(i), subnode);
+            if (_ret != null)
+                return _ret;
+        }
+        return null;
+    }
 
-	/// <summary>
-	/// 查找子对象
-	/// </summary>
-	static public Transform ChildTrsf(UObject uobj, string subnode) {
+    static public GameObject ChildRecursion(UObject uobj, string subnode)
+    {
+        Transform trsf = ChildRecursionTrsf(uobj, subnode);
+        return trsf?.gameObject;
+    }
+
+    /// <summary>
+    /// 查找子对象
+    /// </summary>
+    static public Transform ChildTrsf(UObject uobj, string subnode)
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return null;
-		return trsf.Find(subnode);
-	}
+        return trsf.Find(subnode);
+    }
 
-	static public GameObject Child(UObject uobj, string subnode) {
-		Transform tf = ChildTrsf(uobj, subnode);
-		if(IsNull(tf)) return null;
-		return tf.gameObject;
-	}
+    static public GameObject Child(UObject uobj, string subnode)
+    {
+        Transform tf = ChildTrsf(uobj, subnode);
+        if (IsNull(tf)) return null;
+        return tf.gameObject;
+    }
 
-	/// <summary>
-	/// 取平级对象
-	/// </summary>
-	static public GameObject Peer(UObject uobj, string subnode) {
+    /// <summary>
+    /// 取平级对象
+    /// </summary>
+    static public GameObject Peer(UObject uobj, string subnode)
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return null;
-		return Child(trsf.parent,subnode);
-	}
-	
-	static public GameObject GetGobj(string name,bool isNew,bool isNoDestroy) {
-		GameObject gobj = GameObject.Find(name);
-		if (isNew && IsNull(gobj)) {
-			gobj = new GameObject(name);
-		}
-		if(isNoDestroy) GameObject.DontDestroyOnLoad (gobj);
-		return gobj;
-	}
+        return Child(trsf.parent, subnode);
+    }
 
-	static public GameObject GetGobj(string name,bool isNew) {
-		return GetGobj(name,isNew,false);
-	}
+    static public GameObject GetGobj(string name, bool isNew, bool isNoDestroy)
+    {
+        GameObject gobj = GameObject.Find(name);
+        if (isNew && IsNull(gobj))
+        {
+            gobj = new GameObject(name);
+        }
+        if (isNoDestroy) GameObject.DontDestroyOnLoad(gobj);
+        return gobj;
+    }
 
-	static public GameObject GetGobj(string name) {
-		return GetGobj(name,true);
-	}
+    static public GameObject GetGobj(string name, bool isNew)
+    {
+        return GetGobj(name, isNew, false);
+    }
 
-	static public GameObject GetGobjNotDestroy(string name) {
-		return GetGobj(name,true,true);
-	}
+    static public GameObject GetGobj(string name)
+    {
+        return GetGobj(name, true);
+    }
 
-	//设置子物体显示隐藏（不包括父物体本身）
-	static public void SetChildActive(UObject uobj, bool isActive)
-	{
+    static public GameObject GetGobjNotDestroy(string name)
+    {
+        return GetGobj(name, true, true);
+    }
+
+    //设置子物体显示隐藏（不包括父物体本身）
+    static public void SetChildActive(UObject uobj, bool isActive)
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return;
-		int lens = trsf.childCount;
-		GameObject _go_;
-		for (int i = 0; i < lens; i++)
-		{
-			_go_ = trsf.GetChild(i).gameObject;
-			_go_.SetActive(isActive);
-		}
-	}
+        int lens = trsf.childCount;
+        GameObject _go_;
+        for (int i = 0; i < lens; i++)
+        {
+            _go_ = trsf.GetChild(i).gameObject;
+            _go_.SetActive(isActive);
+        }
+    }
 
-	/// <summary>
-	/// 设置父节点
-	/// </summary>
-	static public void SetParent(UObject uobj, UObject uobjParent, bool isLocalZero) {
+    /// <summary>
+    /// 设置父节点
+    /// </summary>
+    static public void SetParent(UObject uobj, UObject uobjParent, bool isLocalZero)
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return;
         Transform trsfParent = ToTransform(uobjParent);
-        trsf.SetParent (trsfParent,!isLocalZero);
-		// trsf.parent = trsfParent;
-		
-		if(isLocalZero){
-			trsf.localPosition = Vector3.zero;
-			trsf.localEulerAngles = Vector3.zero;
-			trsf.localScale = Vector3.one;
-		}
-	}
+        trsf.SetParent(trsfParent, !isLocalZero);
+        // trsf.parent = trsfParent;
 
-	static public void SetParent(UObject uobj, UObject uobjParent) {
-		SetParent(uobj, uobjParent, true); 
-	}
+        if (isLocalZero)
+        {
+            trsf.localPosition = Vector3.zero;
+            trsf.localEulerAngles = Vector3.zero;
+            trsf.localScale = Vector3.one;
+        }
+    }
 
-	static public void SetParentSyncLayer(UObject uobj, UObject uobjParent, bool isLocalZero) {
+    static public void SetParent(UObject uobj, UObject uobjParent)
+    {
+        SetParent(uobj, uobjParent, true);
+    }
+
+    static public void SetParentSyncLayer(UObject uobj, UObject uobjParent, bool isLocalZero)
+    {
         GameObject gobj = ToGObj(uobj);
         GameObject gobjParent = ToGObj(uobjParent);
         SetParent(gobj, gobjParent, isLocalZero);
-		if(IsNoNull(gobj) && IsNoNull(gobjParent)){
-			int layer = gobjParent.layer;
-			SetLayerBy(gobj, layer,true);
-		}
-	}
+        if (IsNoNull(gobj) && IsNoNull(gobjParent))
+        {
+            int layer = gobjParent.layer;
+            SetLayerBy(gobj, layer, true);
+        }
+    }
 
-	static public GameObject Clone(UObject uobj, UObject uobjParent) {
+    static public GameObject Clone(UObject uobj, UObject uobjParent)
+    {
         GameObject gobj = ToGObj(uobj);
         if (IsNull(gobj)) return null;
         Transform trsfParent = ToTransform(uobjParent);
         GameObject ret = GameObject.Instantiate(gobj, trsfParent, false) as GameObject;
-		if(IsNoNull(trsfParent)){
-			SetParentSyncLayer(ret.transform, trsfParent, true);
-		}
-		return ret;
-	}
+        if (IsNoNull(trsfParent))
+        {
+            SetParentSyncLayer(ret.transform, trsfParent, true);
+        }
+        return ret;
+    }
 
-	static public GameObject Clone(UObject uobj) {
+    static public GameObject Clone(UObject uobj)
+    {
         Transform trsf = ToTransform(uobj);
-        return Clone (trsf, trsf?.parent);
-	}
+        return Clone(trsf, trsf?.parent);
+    }
 
     static public void SetLayer(UObject uobj, int layer)
     {
@@ -358,88 +407,104 @@ public class GHelper {
         gobj.layer = layer;
     }
 
-    static public void SetLayer(UObject uobj, string nmLayer){
+    static public void SetLayer(UObject uobj, string nmLayer)
+    {
         GameObject gobj = ToGObj(uobj);
         if (IsNull(gobj)) return;
-		int layer = LayerMask.NameToLayer(nmLayer);
-		gobj.layer = layer;
-	}
+        int layer = LayerMask.NameToLayer(nmLayer);
+        gobj.layer = layer;
+    }
 
-	static public void SetLayerAll(UObject uobj, int layer){
+    static public void SetLayerAll(UObject uobj, int layer)
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return;
-		SetLayer(trsf,layer);
+        SetLayer(trsf, layer);
 
-		int lens = trsf.childCount;
-		for (int i = 0; i < lens; i++)
-		{
-			SetLayerAll(trsf.GetChild(i),layer);
-		}
-	}
+        int lens = trsf.childCount;
+        for (int i = 0; i < lens; i++)
+        {
+            SetLayerAll(trsf.GetChild(i), layer);
+        }
+    }
 
-	static public void SetLayerAll(UObject uobj, string nmLayer){
+    static public void SetLayerAll(UObject uobj, string nmLayer)
+    {
         Transform trsf = ToTransform(uobj);
         if (IsNull(trsf)) return;
-		int layer = LayerMask.NameToLayer(nmLayer);
-		SetLayerAll(trsf,layer);
-	}
+        int layer = LayerMask.NameToLayer(nmLayer);
+        SetLayerAll(trsf, layer);
+    }
 
-	static public void SetLayerBy(UObject uobj, string nmLayer,bool isAll){
+    static public void SetLayerBy(UObject uobj, string nmLayer, bool isAll)
+    {
         GameObject gobj = ToGObj(uobj);
-        if (isAll){
-			SetLayerAll(gobj,nmLayer);
-		}else{
-			SetLayer(gobj,nmLayer);
-		}
-	}
+        if (isAll)
+        {
+            SetLayerAll(gobj, nmLayer);
+        }
+        else
+        {
+            SetLayer(gobj, nmLayer);
+        }
+    }
 
-	static public void SetLayerBy(UObject uobj, int layer,bool isAll){
+    static public void SetLayerBy(UObject uobj, int layer, bool isAll)
+    {
         GameObject gobj = ToGObj(uobj);
-        if (isAll){
-			SetLayerAll(gobj,layer);
-		}else{
-			SetLayer(gobj,layer);
-		}
-	}
+        if (isAll)
+        {
+            SetLayerAll(gobj, layer);
+        }
+        else
+        {
+            SetLayer(gobj, layer);
+        }
+    }
 
-	static public Vector3 ToVec3(float x,float y,float z){
-		return new Vector3(x,y,z);
-	}
+    static public Vector3 ToVec3(float x, float y, float z)
+    {
+        return new Vector3(x, y, z);
+    }
 
-	static public void GetRectSize(UObject uobj, ref float w,ref float h) {
-		w = 0;h = 0;
+    static public void GetRectSize(UObject uobj, ref float w, ref float h)
+    {
+        w = 0; h = 0;
         RectTransform _r = ToRectTransform(uobj);
         if (IsNull(_r)) return;
-		var v2 = _r.rect.size;
-		w = v2.x;
-		h = v2.y;
-	}
+        var v2 = _r.rect.size;
+        w = v2.x;
+        h = v2.y;
+    }
 
-	// Relative 相对
-	static public void RecursionName(UObject uobj, ref string refName)
-	{
-        Transform trsf = ToTransform(uobj);
-        if (!trsf){
-			return;
-		}
-
-		if(string.IsNullOrEmpty(refName))
-		{
-			refName = trsf.name;
-		} else {
-			refName = trsf.name + "/" + refName;
-		}
-
-		RecursionName(trsf.parent,ref refName);
-	}
-
-	static public string RelativeName(UObject uobj)
+    // Relative 相对
+    static public void RecursionName(UObject uobj, ref string refName)
     {
-		string ret = "";
         Transform trsf = ToTransform(uobj);
-        RecursionName(trsf,ref ret);
-		return ret;
-	}
+        if (!trsf)
+        {
+            return;
+        }
+
+        if (string.IsNullOrEmpty(refName))
+        {
+            refName = trsf.name;
+        }
+        else
+        {
+            refName = trsf.name + "/" + refName;
+        }
+
+        RecursionName(trsf.parent, ref refName);
+    }
+
+    static public string RelativeName(UObject uobj)
+    {
+        string ret = "";
+        Transform trsf = ToTransform(uobj);
+        RecursionName(trsf, ref ret);
+        return ret;
+    }
 
     static public int NMaxMore(params int[] vals)
     {
@@ -542,7 +607,7 @@ public class GHelper {
         if (IsNull(obj)) return false;
         return obj is PrefabBasic;
     }
-    
+
     /// <summary>
     /// 网络可用
     /// </summary>
