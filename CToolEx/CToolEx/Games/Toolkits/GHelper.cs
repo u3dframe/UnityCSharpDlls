55cab4edecd2068b3fcf6f2f76a706d5c1694351
龚阳辉 <gongyanghui@dianyuegame.com>
2020-12-19 11:02:12 +0800

    移动函数，到公共对象里面

--- CToolEx/CToolEx/Games/Toolkits/GHelper.cs
+++ CToolEx/CToolEx/Games/Toolkits/GHelper.cs
@@ -10,7 +10,7 @@ using UObject = UnityEngine.Object;
 /// 日期 : 2020-06-26 09:33
 /// 功能 : 泛型是不能被Tolua导成函数的
 /// </summary>
-public class GHelper
+public class GHelper : Core.Kernel.ObjEx
 {
     static public bool Is_App_Quit = false;
     static public readonly DateTime DT_Start = new DateTime(1970, 1, 1, 0, 0, 0);
@@ -503,77 +503,6 @@ public class GHelper
         string ret = "";
         Transform trsf = ToTransform(uobj);
         RecursionName(trsf, ref ret);
-        return ret;
-    }
-
-    static public int NMaxMore(params int[] vals)
-    {
-        if (vals == null || vals.Length <= 0) return 0;
-        int max = vals[0];
-        for (int i = 1; i < vals.Length; i++)
-        {
-            if (max < vals[i])
-            {
-                max = vals[i];
-            }
-        }
-        return max;
-    }
-    static public int NMax(int v1, int v2, int v3)
-    {
-        return NMaxMore(v1, v2, v3);
-    }
-
-    static public int NMax(int v1, int v2, int v3, int v4)
-    {
-        return NMaxMore(v1, v2, v3, v4);
-    }
-
-    static public double ToDecimal(double org, int acc, bool isRound)
-    {
-        double pow = 1;
-        for (int i = 0; i < acc; i++)
-        {
-            pow *= 10;
-        }
-
-        double temp = org * pow;
-        if (isRound)
-        {
-            temp += 0.5;
-        }
-
-        return ((int)temp) / pow;
-    }
-
-    static public float Round(double org, int acc)
-    {
-        return (float)ToDecimal(org, acc, true);
-    }
-
-    static public float Round(float org, int acc)
-    {
-        return (float)ToDecimal(org, acc, true);
-    }
-
-    static public int Str2Int(string str)
-    {
-        int ret = 0;
-        int.TryParse(str, out ret);
-        return ret;
-    }
-
-    static public long Str2Long(string str)
-    {
-        long ret = 0;
-        long.TryParse(str, out ret);
-        return ret;
-    }
-
-    static public float Str2Float(string str)
-    {
-        float ret = 0;
-        float.TryParse(str, out ret);
         return ret;
     }
 
--- CToolEx/CToolEx/Games/Toolkits/ObjEx.cs
+++ CToolEx/CToolEx/Games/Toolkits/ObjEx.cs
@@ -20,5 +20,76 @@
             int lens = LensArrs(arrs);
             return lens <= 0;
         }
+
+        static public int NMaxMore(params int[] vals)
+        {
+            if (vals == null || vals.Length <= 0) return 0;
+            int max = vals[0];
+            for (int i = 1; i < vals.Length; i++)
+            {
+                if (max < vals[i])
+                {
+                    max = vals[i];
+                }
+            }
+            return max;
+        }
+        static public int NMax(int v1, int v2, int v3)
+        {
+            return NMaxMore(v1, v2, v3);
+        }
+
+        static public int NMax(int v1, int v2, int v3, int v4)
+        {
+            return NMaxMore(v1, v2, v3, v4);
+        }
+
+        static public double ToDecimal(double org, int acc, bool isRound)
+        {
+            double pow = 1;
+            for (int i = 0; i < acc; i++)
+            {
+                pow *= 10;
+            }
+
+            double temp = org * pow;
+            if (isRound)
+            {
+                temp += 0.5;
+            }
+
+            return ((int)temp) / pow;
+        }
+
+        static public float Round(double org, int acc)
+        {
+            return (float)ToDecimal(org, acc, true);
+        }
+
+        static public float Round(float org, int acc)
+        {
+            return (float)ToDecimal(org, acc, true);
+        }
+
+        static public int Str2Int(string str)
+        {
+            int ret = 0;
+            int.TryParse(str, out ret);
+            return ret;
+        }
+
+        static public long Str2Long(string str)
+        {
+            long ret = 0;
+            long.TryParse(str, out ret);
+            return ret;
+        }
+
+        static public float Str2Float(string str)
+        {
+            float ret = 0;
+            float.TryParse(str, out ret);
+            return ret;
+        }
     }
 }
