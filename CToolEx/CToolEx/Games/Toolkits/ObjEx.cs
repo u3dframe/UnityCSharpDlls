0b1addab90d36109e8a3608835dd7ecd1da9341d
龚阳辉 <gongyanghui@dianyuegame.com>
2020-12-19 10:06:25 +0800

    抽离脚本

---
+++ CToolEx/CToolEx/Games/Toolkits/ObjEx.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Kernel
+{
+    /// <summary>
+    /// 类名 : 对象工具
+    /// 作者 : Canyon / 龚阳辉
+    /// 日期 : 2016-12-23 15:20
+    /// 功能 : 
+    /// </summary>
+    public class ObjEx
+    {
+        static public int LensArrs(object[] arrs)
+        {
+            if (arrs == null)
+                return 0;
+            return arrs.Length;
+        }
+
+        static public bool IsNullOrEmpty(object[] arrs)
+        {
+            int lens = LensArrs(arrs);
+            return lens <= 0;
+        }
+    }
+}
