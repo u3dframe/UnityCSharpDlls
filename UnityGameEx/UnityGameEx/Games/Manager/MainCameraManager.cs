0ea631966f154d4013f8b54e5cbe461d17412cf3
龚阳辉 <gongyanghui@dianyuegame.com>
2020-12-23 11:01:39 +0800

    设置pplay对象

--- UnityGameEx/UnityGameEx/Games/Manager/MainCameraManager.cs
+++ UnityGameEx/UnityGameEx/Games/Manager/MainCameraManager.cs
@@ -29,6 +29,14 @@ public class MainCameraManager : MgrMainCamera
             this.m_postLayer = UtilityHelper.Get<PostProcessLayer>(this.m_camera);
             this.m_postVolume = UtilityHelper.Get<PostProcessVolume>(this.m_camera);
         }
+        this.EnablePPLayer(false);
+    }
+
+    public void EnablePPLayer(bool isEnabled)
+    {
+        if (this.m_postLayer == null)
+            return;
+        this.m_postLayer.enabled = isEnabled;
     }
 
     public void SetPPVolume(PostProcessProfile pppfile)
@@ -36,5 +44,6 @@ public class MainCameraManager : MgrMainCamera
         if (this.m_postVolume == null)
             return;
         this.m_postVolume.profile = pppfile;
+        this.EnablePPLayer(pppfile != null);
     }
 }
