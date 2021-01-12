5b4392c06474f914a44716ec3255cb8b4c3e9f0c
龚阳辉 <gongyanghui@dianyuegame.com>
2021-01-12 15:33:09 +0800

    添加内存枚举

--- CToolEx/CToolEx/Games/Components/MemDisplay.cs
+++ CToolEx/CToolEx/Games/Components/MemDisplay.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Profiling;
+using MemBType = Core.MemBType;
 
 /// <summary>
 /// 类名 : 内存显示
@@ -16,14 +17,17 @@ public class MemDisplay : MonoBehaviour
 
     public long m_outMemAll = 0;
     public long m_outMemFree = 0;
-    public double m_kb = 1024d;
+    public MemBType m_eBType = MemBType.MB;
+    public double m_def_kb = 1024;
+    private double m_kb = 1d;
+    private string m_key_byte = "b";
 
     void OnGUI()
 	{
 		int w = Screen.width, h = Screen.height;
         int _h = (int)(h * hsRate) + 1;
-        Rect rect = new Rect(0, 0, w, _h);
-		style.alignment = TextAnchor.UpperRight;
+        Rect rect = new Rect(0,50, w, _h);
+		style.alignment = TextAnchor.UpperLeft;
 		style.fontSize = _h;
 		style.normal.textColor = tCol;
 
@@ -32,8 +36,28 @@ public class MemDisplay : MonoBehaviour
         long usedHeap = Profiler.usedHeapSizeLong;
         long totalMem = System.GC.GetTotalMemory(false);
 
+        switch (m_eBType)
+        {
+            case MemBType.KB:
+                m_kb = m_def_kb;
+                m_key_byte = "kb";
+                break;
+            case MemBType.MB:
+                m_kb = m_def_kb * m_def_kb;
+                m_key_byte = "mb";
+                break;
+            case MemBType.GB:
+                m_kb = m_def_kb * m_def_kb * m_def_kb;
+                m_key_byte = "gb";
+                break;
+            default:
+                m_kb = 1d;
+                m_key_byte = "b";
+                break;
+        }
 
-        string text = string.Format("MonoAll {0:0.0} k , MonoUsed {1:0.0} k , usedHeap {2:0.0} k , Total Memory {3:0.0} k , OT_Memory {4:0.0} k , OFree_Memory {5:0.0} k"
+        string text = string.Format("M_All {1:0.0} {0} ,M_Used {2:0.0} {0} ,usedHeap {3:0.0} {0} ,GC_Total {4:0.0} {0} ,O_T {5:0.0} {0} ,O_Free{6:0.0} {0}"
+            , m_key_byte
             , (monoAll / m_kb), (monoUsed / m_kb), (usedHeap / m_kb), (totalMem / m_kb)
             , (m_outMemAll / m_kb), (m_outMemFree / m_kb));
 		GUI.Label(rect, text, style);
