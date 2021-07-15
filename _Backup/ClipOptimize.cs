47c33998e21b34abbe8a2714601b9fbe6ba8123f
龚阳辉 <gongyanghui@dianyuegame.com>
2021-07-15 19:11:14 +0800

    fbx文件不能优化，但是创建出来的Clip是可以进行优化的，备份优化Clip的文件

--- _Backup/ClipOptimize.cs
+++ _Backup/ClipOptimize.cs
@@ -17,8 +17,8 @@ public class ClipOptimize : EditorWindow
 {
     private bool m_excludeScale;
 
-    [MenuItem("Tools/Optimise/ClipOpt")]
-    [MenuItem("Assets/Optimise/ClipOpt")]
+    [MenuItem("Tools_Art/Optimise/ClipOpt")]
+    [MenuItem("Assets/Tools_Art/Optimise/ClipOpt")]
     protected static void Open()
     {
         GetWindow<ClipOptimize>();
@@ -27,6 +27,8 @@ public class ClipOptimize : EditorWindow
     private Vector2 m_scoll;
     private bool m_ing;
     private int m_index;
+    static private string m_fpower = "f3";
+    private bool m_isChg;
 
     public void OnGUI()
     {
@@ -43,11 +45,17 @@ public class ClipOptimize : EditorWindow
                 EditorGUILayout.ObjectField(clip, typeof(AnimationClip), false);
             }
         }
-
-
+		using (new EditorGUILayout.HorizontalScope())
+        {
+            m_fpower = EditorGUILayout.TextField("Format Float : ",m_fpower);
+            if(!m_fpower.StartsWith("f"))
+                m_fpower = "f3";
+        }
         using (new EditorGUILayout.HorizontalScope())
         {
             m_excludeScale = EditorGUILayout.ToggleLeft("Exclude Scale", m_excludeScale);
+            if(!m_fpower.StartsWith("f"))
+                m_fpower = "f3";
 
             if (GUILayout.Button("Optimize"))
             {
@@ -63,6 +71,7 @@ public class ClipOptimize : EditorWindow
                 EditorUtility.ClearProgressBar();
                 return;
             }
+            m_isChg = false;
             var info = string.Format("Process {0}/{1}", m_index, selects.Length);
             EditorUtility.DisplayProgressBar("Optimize Clip", info, (m_index + 1f) / selects.Length);
 
@@ -71,12 +80,19 @@ public class ClipOptimize : EditorWindow
             var clip = obj as AnimationClip;
             if (clip == null)
                 return;
-            FixFloatAtClip(clip, m_excludeScale);
+            m_isChg = m_isChg || FixFloatAtClip(clip, m_excludeScale);
+            if (m_isChg)
+            {
+                //EditorUtility.SetDirty(clip);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
         }
     }
 
-    private static void FixFloatAtClip(AnimationClip clip, bool excludeScale)
+    private static bool FixFloatAtClip(AnimationClip clip, bool excludeScale)
     {
+        bool _isOkey = false;
         try
         {
             if (excludeScale)
@@ -87,6 +103,7 @@ public class ClipOptimize : EditorWindow
                     if (name.Contains("scale"))
                     {
                         AnimationUtility.SetEditorCurve(clip, theCurveBinding, null);
+                        _isOkey = true;
                     }
                 }
             }
@@ -103,18 +120,20 @@ public class ClipOptimize : EditorWindow
                 for (var i = 0; i < keyFrames.Length; i++)
                 {
                     var key = keyFrames[i];
-                    key.value = float.Parse(key.value.ToString("f3"));
-                    key.inTangent = float.Parse(key.inTangent.ToString("f3"));
-                    key.outTangent = float.Parse(key.outTangent.ToString("f3"));
+                    key.value = float.Parse(key.value.ToString(m_fpower));
+                    key.inTangent = float.Parse(key.inTangent.ToString(m_fpower));
+                    key.outTangent = float.Parse(key.outTangent.ToString(m_fpower));
                     keyFrames[i] = key;
                 }
                 curve.keys = keyFrames;
                 clip.SetCurve(curveDate.path, curveDate.type, curveDate.propertyName, curve);
+                _isOkey = true;
             }
         }
         catch (System.Exception e)
         {
-            Debug.LogError(string.Format("CompressAnimationClip Failed !!! animationPath : {0} error: {1}", clip.name, e));
+            Debug.LogErrorFormat("CompressAnimationClip Failed !!! animationPath : {0} error: {1}", clip.name, e);
         }
+        return _isOkey;
     }
 }
