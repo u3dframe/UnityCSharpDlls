601983ef0edfe12d1af6319c894387d9e8aeb2db
canyon <gongyanghui1986@163.com>
2022-03-03 20:00:21 +0800

    备份准备拆分SVC

---
+++ _Backup/ED_RecordTime.cs
@@ -0,0 +1,135 @@
+﻿using LitJson;
+/// <summary>
+/// 类名 : 统计时间的脚本
+/// 作者 : Canyon / 龚阳辉
+/// 日期 : 2021-01-06 20:03
+/// 功能 : 
+/// </summary>
+public class ED_RecordTime : Core.Kernel.Beans.ED_Basic
+{
+    static ListDict<ED_RecordTime> _dic = new ListDict<ED_RecordTime>(false);
+    static protected ED_RecordTime Builder(string tag)
+    {
+        ED_RecordTime _ret = GetCache<ED_RecordTime>();
+        if(_ret == null){
+            _ret = new ED_RecordTime();
+        }
+        _ret.Init(tag);
+        return _ret;
+    }
+
+    static public ED_RecordTime GetOrNew(string tag)
+    {
+        if(_dic.ContainsKey(tag))
+            return _dic.Get(tag);
+        return Builder(tag);
+    }
+    
+    static public ED_RecordTime objProgress{
+        get{
+            return GetOrNew("objProgress");
+        }
+    }
+    
+    public ED_RecordTime() : base()
+    {
+    }
+
+    private string m_tag = null;
+    private JsonData jroot = null;
+    private JsonData jList = null;
+    static System.DateTime d1970 = new System.DateTime(1970,1,1);
+
+    protected void Init(string tag){
+        if(jroot == null)
+            jroot = LJsonHelper.NewJObj();
+        if(jList == null)
+            jList = LJsonHelper.NewJArr();
+
+        jList.Clear();
+        jroot.Clear();
+        this.m_tag = tag;
+        jroot["tag"] = tag;
+        jroot["msg_list"] = jList;
+        
+        ED_RecordTime _old = _dic.Remove4Get(tag);
+        if(_old != null){
+            _old.ReBack();
+        }
+        _dic.Add(tag,this);
+    }
+
+    protected void ReBack(){
+        jList.Clear();
+        jroot.Clear();
+        AddCache(this);
+    }
+    
+
+    public ED_RecordTime AddMsg(object msg){
+        if(msg != null){
+            JsonData _cur = LJsonHelper.NewJObj();
+            _cur["msg"] = new JsonData(msg);
+            var dt8 = System.DateTime.UtcNow.AddHours(8);
+            var d1970_8 = d1970.ToUniversalTime().AddHours(8);
+            var _diff = dt8 - d1970_8;
+            _cur["ms"] = (long)(_diff.TotalMilliseconds);
+            _cur["time"] = dt8.ToString("yyyy-MM-dd HH:mm:ss");
+            jList.Add(_cur);
+        }
+        return this;
+    }
+
+    public void EndMsg(object msg,bool isReBack = true){
+        this.AddMsg(msg);
+        string _json = this.jroot.ToJson();
+        if(isReBack)
+            this.ReBack();
+        
+        var dt8 = System.DateTime.UtcNow.AddHours(8);
+        string _fp = string.Format("{0}../{1}_{2}.txt",Core.GameFile.m_dirRes,this.m_tag,dt8.ToString("MMddHHmmss"));
+        Core.GameFile.WriteText(_fp,_json,true);
+    }
+
+    static public void SyDiffMS(string name){
+        string _fp = string.Format("{0}../{1}.txt",Core.GameFile.m_dirRes,name);
+        string _json = Core.GameFile.GetText4File(_fp);
+        var jd = LJsonHelper.ToJData(_json);
+        var jarr = jd["msg_list"];
+        JsonData jIt = null,jItLast = null,jItFirst = null;
+        int lens = jarr.Count;
+        JsonData jNew = LJsonHelper.NewJArr();
+        for (int i = 0; i < lens; i++)
+        {
+            if(jIt != null)
+                jItLast = jIt;
+            jIt = jarr[i];
+            if(jItFirst == null)
+                jItFirst = jIt;
+            if(jItLast != null)
+            {
+                var jNew2 = LJsonHelper.NewJObj();
+                jNew2["A"] = jItLast["msg"].ToJson();
+                jNew2["B"] = jIt["msg"].ToJson();
+                long _diffMs = (long)jIt["ms"] - (long)jItLast["ms"];
+                jNew2["(B - A) = ms"] = _diffMs;
+                jNew2["_diffMs"] = _diffMs;
+                jNew.Add(jNew2);
+            }
+        }
+
+        var jNew3 = LJsonHelper.NewJObj();
+        jNew3["A"] = jItFirst["msg"].ToJson();
+        jNew3["B"] = jIt["msg"].ToJson();
+        long _diffMs1 = (long)jIt["ms"] - (long)jItFirst["ms"];
+        jNew3["(B - A) = ms"] = _diffMs1;
+        jNew3["_diffMs"] = _diffMs1;
+        jNew3["_diffSec"] = _diffMs1 / 1000;
+        jNew.Add(jNew3);
+
+        var dt8 = System.DateTime.UtcNow.AddHours(8);
+        _fp = string.Format("{0}../_diff_ms_{1}.txt",Core.GameFile.m_dirRes,dt8.ToString("MMddHHmmss"));
+        _json = jNew.ToJson();
+        Core.GameFile.WriteText(_fp,_json,true);
+    }
+}
--- _Backup/ShaderVariantCollectionExporter.cs
+++ _Backup/ShaderVariantCollectionExporter.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using Debug = UnityEngine.Debug;
+using SShaderVariant = UnityEngine.ShaderVariantCollection.ShaderVariant;
 
 /// <summary>
 /// 类名 : ShaderVariantCollection 收集工具
@@ -26,77 +27,91 @@ public static class ShaderVariantCollectionExporter
     [MenuItem("Tools/Shader/Export GameSVC")]
     static void Export2()
     {
-        ExportSVC("Assets/_Develop/Builds/all_svc.shadervariants","_Develop/");
+        ExportSVC("Assets/_Develop/Builds/all_svcs.shadervariants", "_Develop/");
     }
 
     /// <summary>
     /// fpSave 以 Assets 开头的要保存的SVC文件的地址
     /// fpDir 是 Assets 资源查找路径地址
     /// </summary>
-    static public void ExportSVC(string fpSave = null, string rootDir = null, bool isLog = false, string fpDir = null)
+    static public void ExportSVC(string fpSave = null, string rootDir = null, string fpDir = null)
     {
-		Debug.LogFormat("=== ExportSVC T0 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
-		if (!string.IsNullOrEmpty(fpSave))
+        EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+        System.Threading.Thread.Sleep(20);
+        Debug.LogFormat("=== ExportSVC T0 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
+        if (!string.IsNullOrEmpty(fpSave))
             _SVCPath = fpSave;
-		
+
         // if (string.IsNullOrEmpty(fpDir))
         //    fpDir = Application.dataPath;
         // var _arrs2 = Directory.GetFiles(fpDir, "*.*", SearchOption.AllDirectories)
         //         .Where(s => s.ToLower().EndsWith(".mat")).ToArray();
 
-        var _arrs2 = AssetDatabase.FindAssets("t:Material"); // t:Material t:Shader
+        EditorUtility.UnloadUnusedAssetsImmediate(true);
+        System.Threading.Thread.Sleep(20);
+        Debug.LogFormat("=== ExportSVC T1 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
+
+        var _arrs2 = AssetDatabase.FindAssets("t:Material"); // t:Material t:Shader        
 
         var materials = new List<Material>();
-        var shaderDict = new Dictionary<Shader, List<Material>>();
-        Shader _shader = null;
-        List<Material> _list = null;
+        var errMats = new HashSet<string>();
         string _pIt;
         Material _mat;
-		bool _isCheckRoot = !string.IsNullOrEmpty(rootDir);
+        Shader _shader = null;
+        bool _isCheckRoot = !string.IsNullOrEmpty(rootDir);
         for (int j = 0; j < _arrs2.Length; j++)
         {
             _pIt = AssetDatabase.GUIDToAssetPath(_arrs2[j]);
             _pIt = _pIt.Substring(_pIt.LastIndexOf("Assets"));
-			_pIt = _pIt.Replace('\\', '/');
-			if(_isCheckRoot && !_pIt.Contains(rootDir))
-				continue;
-			
+            _pIt = _pIt.Replace('\\', '/');
+            if (_isCheckRoot && !_pIt.Contains(rootDir))
+                continue;
+
             _mat = AssetDatabase.LoadAssetAtPath<Material>(_pIt);
             if (_mat != null)
             {
                 _shader = _mat.shader;
                 if (_shader != null)
                 {
-
-                    if (!shaderDict.TryGetValue(_shader, out _list))
+                    if ("Hidden/InternalErrorShader".Equals(_shader.name, System.StringComparison.OrdinalIgnoreCase))
                     {
-                        _list = new List<Material>();
-                        shaderDict.Add(_shader, _list);
+                        errMats.Add(_pIt);
+                        continue;
                     }
 
-                    if (!_list.Contains(_mat))
-                        _list.Add(_mat);
+                    if (!materials.Contains(_mat))
+                        materials.Add(_mat);
                 }
-
-                if (!materials.Contains(_mat))
-                    materials.Add(_mat);
+                else
+                {
+                    errMats.Add(_pIt);
+                }
             }
         }
-		Debug.LogFormat("=== ExportSVC T1 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
+        LogInfo(errMats);
+        Debug.LogFormat("=== ExportSVC T2 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
         ProcessMaterials(materials);
-
-        if (isLog)
-            LogInfo(shaderDict);
     }
 
-    static private void LogInfo(Dictionary<Shader, List<Material>> shaderDict)
+    static private void LogInfo(HashSet<string> errMats)
     {
-        var sb = new System.Text.StringBuilder();
-        foreach (var kvp in shaderDict)
+        if (errMats == null || errMats.Count <= 0)
+            return;
+
+        var sb = new System.Text.StringBuilder("====== error == mat assetPath =\n");
+        foreach (var err in errMats)
         {
-            sb.AppendLine(kvp.Key + " " + kvp.Value.Count + " times");
+            sb.AppendLine("==" + err);
         }
-        Debug.Log(sb.ToString());
+        errMats.Clear();
+
+        var dt8 = System.DateTime.UtcNow.AddHours(8);
+        string _fp = string.Format("{0}/../__err_mat_path_{1}.txt", Application.dataPath, dt8.ToString("MMddHHmmss"));
+        string _cont = sb.ToString();
+        sb.Clear();
+        sb.Length = 0;
+        File.WriteAllText(_fp, _cont);
+        Debug.LogFormat("=== WriteError = [{0}] = [{1}]", _fp, dt8.ToString("HH:mm:ss"));
     }
 
     static private void EditorUpdate()
@@ -104,8 +119,8 @@ public static class ShaderVariantCollectionExporter
         if (_elapsedTime.ElapsedMilliseconds >= WaitTimeBeforeSave)
         {
             EditorApplication.update -= EditorUpdate;
-			EditorApplication.isPlaying = true;
-            System.Threading.Thread.Sleep(15000);
+            EditorApplication.isPlaying = true;
+            System.Threading.Thread.Sleep(20000);
             object _obj = InvokeInternalStaticMethod(TP_CSU, "GetCurrentShaderVariantCollectionVariantCount");
             Debug.LogFormat("=== Update CurrSVC_VariantCount = [{0}] = [{1}]", _obj, System.DateTime.Now.ToString("HH:mm:ss"));
             InvokeInternalStaticMethod(TP_CSU, "SaveCurrentShaderVariantCollection", _SVCPath);
@@ -114,16 +129,22 @@ public static class ShaderVariantCollectionExporter
             _elapsedTime.Stop();
             _elapsedTime.Reset();
             EditorApplication.isPlaying = false;
+
+            if(isReSplitShaderVariants)
+            {
+                _elapsedTime.Start();
+                EditorApplication.update -= _Update4ReSplitSVC;
+                EditorApplication.update += _Update4ReSplitSVC;
+            }
         }
     }
 
     static private void ProcessMaterials(List<Material> materials)
     {
-        EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
         InvokeInternalStaticMethod(TP_CSU, "ClearCurrentShaderVariantCollection");
         object _obj = InvokeInternalStaticMethod(TP_CSU, "GetCurrentShaderVariantCollectionShaderCount");
         Debug.LogFormat("=== CurrSVC_ShaderCount = [{0}] = [{1}]", _obj, System.DateTime.Now.ToString("HH:mm:ss"));
-        
+
         int totalMaterials = materials.Count;
 
         var camera = Camera.main;
@@ -167,16 +188,17 @@ public static class ShaderVariantCollectionExporter
             {
                 x++;
             }
-			if(i > 0 && i % 5 == 0){
-				System.Threading.Thread.Sleep(20);
-			}
+            if (i > 0 && i % 5 == 0)
+            {
+                System.Threading.Thread.Sleep(20);
+            }
         }
-		
-		Debug.LogFormat("=== ExportSVC T3 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
+
+        Debug.LogFormat("=== ExportSVC T3 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
         _elapsedTime.Stop();
         _elapsedTime.Reset();
         _elapsedTime.Start();
-		EditorApplication.isPlaying = false;
+        EditorApplication.isPlaying = false;
         EditorApplication.update -= EditorUpdate;
         EditorApplication.update += EditorUpdate;
     }
@@ -186,18 +208,18 @@ public static class ShaderVariantCollectionExporter
         var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         go.GetComponent<Renderer>().material = material;
         go.transform.position = position;
-        go.name = string.Format("Sphere_{0}|{1}_{2}|{3}", index, x, y, material.name);
+        go.name = string.Format("Sphere_{0:000}|{1:00}_{2:00}|{3}", index, y, x, material.name);
     }
 
     static private object InvokeInternalStaticMethod(System.Type type, string method, params object[] parameters)
     {
         try
         {
-            return _InvokeInternalStaticMethod(type,method,parameters);
+            return _InvokeInternalStaticMethod(type, method, parameters);
         }
         catch (System.Exception ex)
         {
-			Debug.LogErrorFormat("=== {0}.{1} err = \n{2}", type,method,ex);
+            Debug.LogErrorFormat("=== {0}.{1} err = \n{2}", type, method, ex);
         }
         return null;
     }
@@ -214,8 +236,157 @@ public static class ShaderVariantCollectionExporter
         return methodInfo.Invoke(null, parameters);
     }
 	
-	static private System.Type TP_CSU = typeof(ShaderUtil);
+	
+	static private void _Update4ReSplitSVC()
+    {
+        if (_elapsedTime.ElapsedMilliseconds >= 1000)
+        {
+            EditorApplication.update -= _Update4ReSplitSVC;
+            _elapsedTime.Stop();
+            _elapsedTime.Reset();
+			
+			EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+			System.Threading.Thread.Sleep(200);
+			EditorUtility.UnloadUnusedAssetsImmediate(true);
+			System.Threading.Thread.Sleep(200);
+            try
+            {
+                _ReSplitSVC();
+            }
+            finally
+            {
+                if ( File.Exists( _SVCPath ) ) {
+                    if ( !keepTempShaderVariants ) {
+                        AssetDatabase.DeleteAsset( _SVCPath );
+                    }
+                }
+                AssetDatabase.Refresh();
+                AssetDatabase.SaveAssets();
+            }
+        }
+    }
+
+    static private void _ReSplitSVC()
+    {
+        Debug.LogFormat("=== _ReSplitSVC 0 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
+        string _assetPath = _SVCPath;
+        ShaderVariantCollection svcAlls = AssetDatabase.LoadAssetAtPath<ShaderVariantCollection>(_assetPath);
+        if (svcAlls == null)
+            return;
+
+        Dictionary<Shader, List<SShaderVariant>> uiDic = null,ftDic = null;
+        var _svs = GetSShaderVariant(svcAlls,out uiDic,out ftDic);
+        System.Threading.Thread.Sleep(20);
+        Debug.LogFormat("=== _ReSplitSVC 1 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
+        string _uiSvc = "Assets/_Develop/Builds/all_svc.shadervariants";
+        string _fightSvc = "Assets/_Develop/Builds/all_svc_ft.shadervariants";
+        CreateSVC(_uiSvc,uiDic);
+        System.Threading.Thread.Sleep(200);
+        CreateSVC(_fightSvc,ftDic);
+        Debug.LogFormat("=== _ReSplitSVC 2 = [{0}]", System.DateTime.Now.ToString("HH:mm:ss"));
+    }
+
+    static bool _IsInScene(string assetPath){
+        string _ap = assetPath.Replace("\\","/");
+        return _ap.Contains("/Scene/Builds/");
+    }
+
+    static Dictionary<Shader, List<SShaderVariant>> GetSShaderVariant(ShaderVariantCollection svc,out Dictionary<Shader, List<SShaderVariant>> uiDic,out Dictionary<Shader, List<SShaderVariant>> ftDic)
+    {
+        uiDic = new Dictionary<Shader, List<SShaderVariant>>();
+        ftDic = new Dictionary<Shader, List<SShaderVariant>>();
+
+        var shaderVariants = new Dictionary<Shader, List<SShaderVariant>>();
+        using (var so = new SerializedObject(svc))
+        {
+            var array = so.FindProperty("m_Shaders.Array");
+            if (array != null && array.isArray)
+            {
+                var arraySize = array.arraySize;
+                for (int i = 0; i < arraySize; ++i)
+                {
+                    var shaderRef = array.FindPropertyRelative(string.Format("data[{0}].first", i));
+                    var shaderShaderVariants = array.FindPropertyRelative(string.Format("data[{0}].second.variants", i));
+                    if (shaderRef != null && shaderRef.propertyType == SerializedPropertyType.ObjectReference &&
+                        shaderShaderVariants != null && shaderShaderVariants.isArray)
+                    {
+                        var shader = shaderRef.objectReferenceValue as Shader;
+                        if (shader == null)
+                            continue;
+                        var shaderAssetPath = AssetDatabase.GetAssetPath(shader);
+                        List<SShaderVariant> variants = null;
+                        if (!shaderVariants.TryGetValue(shader, out variants))
+                        {
+                            variants = new List<SShaderVariant>();
+                            shaderVariants.Add(shader, variants);
+                        }
+                        var variantCount = shaderShaderVariants.arraySize;
+                        for (int j = 0; j < variantCount; ++j)
+                        {
+                            var prop_keywords = shaderShaderVariants.FindPropertyRelative(string.Format("Array.data[{0}].keywords", j));
+                            var prop_passType = shaderShaderVariants.FindPropertyRelative(string.Format("Array.data[{0}].passType", j));
+                            if (prop_keywords != null && prop_passType != null && prop_keywords.propertyType == SerializedPropertyType.String)
+                            {
+                                var srcKeywords = prop_keywords.stringValue;
+                                if(string.IsNullOrWhiteSpace(srcKeywords))
+                                    continue;
+                                var keywords = srcKeywords.Split(' ');
+                                if(keywords == null || keywords.Length <= 0)
+                                    continue;
+                                
+                                var pathType = (UnityEngine.Rendering.PassType)prop_passType.intValue;
+                                variants.Add(new SShaderVariant(shader, pathType, keywords));
+                            }
+                        }
+
+                        if(_IsInScene(shaderAssetPath)){
+                            if(!ftDic.ContainsKey(shader))
+                            {
+                                ftDic.Add(shader,variants);
+                            }
+                        }else{
+                            if(!uiDic.ContainsKey(shader))
+                            {
+                                uiDic.Add(shader,variants);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return shaderVariants;
+    }
+
+    static ShaderVariantCollection CreateSVC(string svcPath)
+    {
+        var va = AssetDatabase.LoadAssetAtPath<ShaderVariantCollection>( svcPath );
+        if (va == null) {
+            va = new ShaderVariantCollection();
+            AssetDatabase.CreateAsset( va, svcPath );
+            AssetDatabase.Refresh();
+            va = AssetDatabase.LoadAssetAtPath<ShaderVariantCollection>( svcPath );
+        }
+        return va;
+    }
+
+    static void CreateSVC(string svcPath,Dictionary<Shader, List<SShaderVariant>> dic)
+    {
+        var va = CreateSVC(svcPath);
+        va.Clear();
+        foreach(var it in dic){
+            var _list = it.Value;
+            foreach (var _sv in _list)
+            {
+                va.Add( _sv );
+            }
+        }
+        EditorUtility.SetDirty( va );
+    }
+
+    static private System.Type TP_CSU = typeof(ShaderUtil);
     static private readonly Stopwatch _elapsedTime = new Stopwatch();
     private const int WaitTimeBeforeSave = 20000;
     static private string _SVCPath = "Assets/ShaderVariantCollection.shadervariants";
+    static private readonly bool isReSplitShaderVariants = false;
+    static private readonly bool keepTempShaderVariants = true;
 }
