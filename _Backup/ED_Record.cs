a0902e0cacc6d0ddcb3535e97dce43b5ef392c65
canyon <gongyanghui1986@163.com>
2022-06-06 16:09:28 +0800

    数据记录备份文件

--- _Backup/ED_Record.cs
+++ _Backup/ED_Record.cs
@@ -1,51 +1,51 @@
 ﻿using LitJson;
-using Core.Kernel;
-using Core.Kernel.Beans;
+using UnityEngine;
+using System.Collections.Generic;
+using SFile = System.IO.File;
+
 /// <summary>
 /// 类名 : 统计时间的脚本
 /// 作者 : Canyon / 龚阳辉
 /// 日期 : 2021-01-06 20:03
 /// 功能 : 
 /// </summary>
-public class ED_RecordTime : ED_Basic
+public class ED_Record
 {
-    static ListDict<ED_RecordTime> _dic = new ListDict<ED_RecordTime>(false);
-    static protected ED_RecordTime Builder(string tag)
+    static Dictionary<string,ED_Record> _dic = new Dictionary<string,ED_Record>(4);
+    static protected ED_Record Builder(string tag)
     {
-        ED_RecordTime _ret = GetCache<ED_RecordTime>();
-        if(_ret == null){
-            _ret = new ED_RecordTime();
-        }
+        ED_Record _ret = new ED_Record();
         _ret.Init(tag);
         return _ret;
     }
 
-    static public ED_RecordTime GetOrNew(string tag)
+    static public ED_Record GetOrNew(string tag)
     {
-        if(_dic.ContainsKey(tag))
-            return _dic.Get(tag);
+        ED_Record _ret = null;
+        if(_dic.TryGetValue(tag,out _ret))
+            return _ret;
         return Builder(tag);
     }
     
-    static public ED_RecordTime objProgress{
+    static public ED_Record objProgress{
         get{
             return GetOrNew("objProgress");
         }
     }
-	
-	static public ED_RecordTime luaUsed{
+
+    static public ED_Record luaUsed{
         get{
             return GetOrNew("luaUsed");
         }
     }
 
-    static public ED_RecordTime luaNoUse{
+    static public ED_Record luaNoUse{
         get{
             return GetOrNew("luaNoUse");
         }
     }
     
-    public ED_RecordTime() : base()
+    public ED_Record() 
     {
     }
 
@@ -85,8 +85,9 @@ public class ED_RecordTime : ED_Basic
         jroot["tag"] = tag;
         jroot["msg_list"] = jList;
         
-        ED_RecordTime _old = _dic.Remove4Get(tag);
-        if(_old != null){
+        ED_Record _old = null;
+        if(_dic.TryGetValue(tag,out _old))
+        {
             _old.ReBack();
         }
         _dic.Add(tag,this);
@@ -95,7 +96,6 @@ public class ED_RecordTime : ED_Basic
     protected void ReBack(){
         jList.Clear();
         jroot.Clear();
-        AddCache(this);
     }
     
     public ED_Record AddMsg(string msg){
@@ -134,7 +134,8 @@ public class ED_RecordTime : ED_Basic
         }
 
         JsonData _cur = NewJObj();
-        _cur["msg"] = new JsonData(msg);
+        string json = JsonMapper.ToJson(msg);
+        _cur["msg"] = JsonMapper.ToObject(json);
         var dt8 = System.DateTime.UtcNow.AddHours(8);
         var _diff = dt8 - d1970_8;
         _cur["ms"] = (long)(_diff.TotalMilliseconds);
@@ -150,18 +151,21 @@ public class ED_RecordTime : ED_Basic
             this.ReBack();
         
         var dt8 = System.DateTime.UtcNow.AddHours(8);
-        string _fp = string.Format("{0}../{1}_{2}.txt",UGameFile.m_dirRes,this.m_tag,dt8.ToString("MMddHHmmss"));
-        UGameFile.WriteText(_fp,_json,true);
+        string _dir = Application.dataPath + "/";
+        string _fp = string.Format("{0}../../{1}_{2}.txt",_dir,this.m_tag,dt8.ToString("MMddHHmmss"));
+        SFile.WriteAllText(_fp,_json);
+        Debug.LogError(_fp);
     }
 
     static public void SyDiffMS(string name){
-        string _fp = string.Format("{0}../{1}.txt", UGameFile.m_dirRes,name);
-        string _json = UGameFile.GetText4File(_fp);
-        var jd = LJsonHelper.ToJData(_json);
+        string _dir = Application.dataPath + "/";
+        string _fp = string.Format("{0}../../{1}.txt", _dir,name);
+        string _json = SFile.ReadAllText( _fp );
+        var jd = JsonMapper.ToObject(_json);
         var jarr = jd["msg_list"];
         JsonData jIt = null,jItLast = null,jItFirst = null;
         int lens = jarr.Count;
-        JsonData jNew = LJsonHelper.NewJArr();
+        JsonData jNew = NewJArr();
         for (int i = 0; i < lens; i++)
         {
             if(jIt != null)
@@ -171,7 +175,7 @@ public class ED_RecordTime : ED_Basic
                 jItFirst = jIt;
             if(jItLast != null)
             {
-                var jNew2 = LJsonHelper.NewJObj();
+                var jNew2 = NewJObj();
                 jNew2["A"] = jItLast["msg"].ToJson();
                 jNew2["B"] = jIt["msg"].ToJson();
                 long _diffMs = (long)jIt["ms"] - (long)jItLast["ms"];
@@ -181,7 +185,7 @@ public class ED_RecordTime : ED_Basic
             }
         }
 
-        var jNew3 = LJsonHelper.NewJObj();
+        var jNew3 = NewJObj();
         jNew3["A"] = jItFirst["msg"].ToJson();
         jNew3["B"] = jIt["msg"].ToJson();
         long _diffMs1 = (long)jIt["ms"] - (long)jItFirst["ms"];
@@ -191,9 +195,9 @@ public class ED_RecordTime : ED_Basic
         jNew.Add(jNew3);
 
         var dt8 = System.DateTime.UtcNow.AddHours(8);
-        _fp = string.Format("{0}../_diff_ms_{1}.txt",UGameFile.m_dirRes,dt8.ToString("MMddHHmmss"));
+        _fp = string.Format("{0}../../_diff_ms_{1}.txt",_dir,dt8.ToString("MMddHHmmss"));
         _json = jNew.ToJson();
-        UGameFile.WriteText(_fp,_json,true);
+        SFile.WriteAllText(_fp,_json);
 		Debug.LogError(_fp);
     }
 }
