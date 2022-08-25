c6d2e953c7cc19584d349a1d5a4292f401bdf912
canyon <gongyanghui1986@163.com>
2022-08-25 08:35:48 +0800

    更新后上次

--- CToolEx/CToolEx/Games/Components/Bridge/EUP_Bridge.cs
+++ CToolEx/CToolEx/Games/Components/Bridge/EUP_Bridge.cs
@@ -1,91 +1,130 @@
-﻿using UnityEngine;
-using Core.Kernel;
-
-
-/// <summary>
-/// 类名 : 通讯回调函数
-/// 作者 : Canyon / 龚阳辉
-/// 日期 : 2016-05-22 10:15
-/// 功能 : data = json 统一结构，{code = 状态(success,fails),data = [json_str 数据],msg = 提示消息}
-/// [json_str 数据] = {cmd,...}
-/// </summary>
-public delegate void DF_CBBridge(string data);
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core;
 
 /// <summary>
-/// 类名 : U3D 与 平台 通讯
+/// 类名 : U3D 与 平台的通信
 /// 作者 : Canyon / 龚阳辉
 /// 日期 : 2016-05-22 10:15
-/// 功能 : 
-/// 修改 : 抽为父类，开发模式中继承并实现，取消宏 UNITY_ANDROID , UNITY_IOS
+/// 功能 : Android,IOS等
+/// 修改 : 重新整合 2022-04-17 15:35
 /// </summary>
-public class EUP_BasicBridge<T> : MonoSingleton<T> where T : EUP_BasicBridge<T>
+public class EUP_Bridge : EUP_BasicBridge<EUP_Bridge>
 {
-    protected DF_CBBridge _callBack = null;
-    protected string _className = null;
-    protected const string NM_ON_RESULT_FUNC = "OnCB4RetBridge";
-
-    protected override void OnCall4Destroy()
-    {
-        DF_CBBridge _call = this._callBack;
-        this._callBack = null;
-        if (_isMustNewWhenDestroy)
-        {
-            curInstance.Init(_call, this._className);
-        }
+	static public void InitBase(DF_CBBridge onResult){
+		InitBridge(onResult,"");
     }
 
-    public void Init(DF_CBBridge onResult, string className)
+	static public void Send(string param){
+		SendBridge(param);
+    }
+	
+	static public void SendAndCall(string param, DF_CBBridge onResult,string jclassListener = "")
+	{
+		SendBridgeAndCall(param,onResult,jclassListener);
+	}
+
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+    const string NM_JAVA_BRIDGE_CLASS = "com.sdkplugin.bridge.U3DBridge";
+    const string NM_JAVA_METHOD_INITALL = "initAll";
+    const string NM_JAVA_METHOD_INITPARS = "initPars";
+    const string NM_JAVA_METHOD_NOTIFY = "request";
+    protected Dictionary<string,object> dicJo = new Dictionary<string, object>();
+	AndroidJavaClass _jcBridge = null;
+	string _clsListener = "";
+    AndroidJavaClass jcBridge{ get { if( _jcBridge == null ) _jcBridge = new AndroidJavaClass( NM_JAVA_BRIDGE_CLASS ); return _jcBridge; } }
+
+    AndroidJavaObject GetListener(string className){
+		this._clsListener = className;
+		if(dicJo.ContainsKey(className)){
+			return (AndroidJavaObject)dicJo[className];
+		}
+		AndroidJavaObject jo = null;
+		try{
+			AndroidJavaClass _jc = new AndroidJavaClass( className );
+			jo = _jc.CallStatic<AndroidJavaObject>("getInstance");
+		}catch{
+			jo = null;
+		}
+		try{
+			if(jo == null){
+				jo = new AndroidJavaObject(className);
+			}
+			dicJo.Add(className,jo);
+		}catch{
+		}
+		return jo;
+	}
+    public override E CallBridge<E>(bool isStatic, string className, string methodName, params object[] args)
     {
-        this._callBack = onResult;
-        this._className = className;
-        this.OnInit();
+		try{
+			if(isStatic){
+				AndroidJavaClass _jc = new AndroidJavaClass( className );
+				return _jc.CallStatic<E>(methodName,args);
+			}
+
+			AndroidJavaObject jo = GetListener(className);
+			if(jo != null)
+				return jo.Call<E>(methodName,args);
+		}catch{
+		}
+        return base.CallBridge<E>(isStatic, className, methodName, args);
     }
 
-    protected virtual void OnInit()
+	void Init4Android() 
+	{
+		string className  = this._className;
+		if(string.IsNullOrEmpty(className)){
+			jcBridge.CallStatic(NM_JAVA_METHOD_INITPARS,NM_Gobj,NM_ON_RESULT_FUNC);
+			return;
+		}
+		if(className.Equals(this._clsListener))
+			return;
+		
+		AndroidJavaObject joListener = GetListener(className);
+
+		if(joListener == null){
+			jcBridge.CallStatic(NM_JAVA_METHOD_INITPARS,NM_Gobj,NM_ON_RESULT_FUNC);
+			return;
+		}
+		jcBridge.CallStatic(NM_JAVA_METHOD_INITALL,joListener,NM_Gobj,NM_ON_RESULT_FUNC);
+	}
+
+	void Send4Android(string data)
+	{
+		jcBridge.CallStatic(NM_JAVA_METHOD_NOTIFY, data);
+	}
+#elif UNITY_IOS
+	void Init4IOS()
+	{
+	}
+
+	void Send4IOS(string data)
+	{
+	}
+#endif
+
+    protected override void OnInit()
     {
+        base.OnInit();
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+		Init4Android();
+#elif UNITY_IOS
+		Init4IOS();
+#endif
     }
 
-    protected virtual void OnCB4RetBridge(string data)
+    protected override void Send4Bridge(string param)
     {
-        if (_callBack != null)
-            _callBack(data);
-        else
-            Debug.LogWarning("OnCB4RetBridge: _callBack is null");
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+		Send4Android(param);
+#elif UNITY_IOS
+		Send4IOS(param);
+#endif
     }
 
-    protected virtual void Send4Bridge(string param)
-    {
-    }
-
-    public virtual E CallBridge<E>(bool Static,string className, string methodName,params object[] args)
-    {
-        return default(E);
-    }
-
-    public void AppendCall(DF_CBBridge onCall)
-    {
-        this._callBack -= onCall;
-        this._callBack += onCall;
-    }
-
-    static public void InitBridge(DF_CBBridge onResult, string jclassListener)
-    {
-        if (UGameFile.m_isEditor)
-            return;
-        curInstance.Init(onResult, jclassListener);
-    }
-
-    static public void SendBridge(string param)
-    {
-        if (UGameFile.m_isEditor)
-            return;
-
-        curInstance.Send4Bridge(param);
-    }
-
-    static public void SendBridgeAndCall(string param, DF_CBBridge onResult, string jclassListener = "")
-    {
-        InitBridge(onResult,jclassListener);
-        SendBridge(param);
-    }
 }
